657b83e4c36f5183c2d961ffd4b1dcf123b12efb
Stephen Sherratt <stephen@sherra.tt>
2022-04-10 00:36:39 +1000

    Flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "MSPaint95";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell rec {
+          buildInputs = [
+            python3
+            nodejs-17_x
+            nodePackages.typescript-language-server
+          ];
+        };
+      }
+    );
+}
